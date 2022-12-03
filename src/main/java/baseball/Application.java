da2ba592fcd1f988b8df4232e379520357d9247f
jini_11 <jieun21124@naver.com>
2022-12-03 13:15:58 +0900

    docs: 기능 목록 작성

--- src/main/java/baseball/Application.java
+++
@@ -1,8 +0,0 @@
-package baseball;
-
-public class Application {
-    public static void main(String[] args) {
-        BaseballGame baseballGame = new BaseballGame();
-        baseballGame.start();
-    }
-}
